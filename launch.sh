f0a888216fba12152ce23cc57c31c00f41b2f0e2
Julien-Somasundaram <somasundaramjulien@gmail.com>
2024-06-23 18:10:01 +0200

    multipassv3

--- launch.sh
+++ launch.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 git clone https://github.com/Julien-Somasundaram/GraphQL.git ./AWS/MyApp
 docker stack deploy -c ./AWS/docker-stack.yml myapp
--- multipass.sh
+++ multipass.sh
@@ -1,13 +1,25 @@
-multipass launch --name manager
-multipass launch --name worker1
-multipass launch --name worker2
+#!/bin/bash
 
-multipass exec manager -- docker swarm init
-# recuperer le token pour les workers
+# Créer des instances Multipass
+multipass launch --name manager -c 2 -m 2G -d 10G
+multipass launch --name worker1 -c 2 -m 2G -d 10G
+multipass launch --name worker2 -c 2 -m 2G -d 10G
+
+# Installer Docker sur les instances
+multipass exec manager -- bash -c "curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh"
+multipass exec worker1 -- bash -c "curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh"
+multipass exec worker2 -- bash -c "curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh"
+
+# Initialiser Docker Swarm sur le manager
+multipass exec manager -- bash -c "docker swarm init"
+
+# Récupérer le jeton pour les workers
 output=$(multipass exec manager -- docker swarm join-token worker)
 join_command=$(echo "$output" | grep "docker swarm join" | sed -e 's/^[[:space:]]*//')
-multipass exec worker1 -- $join_command
-multipass exec worker2 -- $join_command
 
-multipass exec manager -- git clone https://github.com/Julien-Somasundaram/AWS.git
-multipass exec manager -- sh ./AWS/launch.sh
+# Joindre les workers au Swarm
+multipass exec worker1 -- bash -c "$join_command"
+multipass exec worker2 -- bash -c "$join_command"
+
+# Cloner le dépôt Git et lancer le script
+multipass exec manager -- bash -c "git clone https://github.com/Julien-Somasundaram/AWS.git && cd AWS && bash launch.sh"
